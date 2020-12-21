b7e520e0be47a51ba3a332621336f1eda0a219e5
Vsept17 <verzchseptian@gmail.com>
2020-12-21 21:39:32 +0700

    fixing conflict

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,8 @@ const recipesRouter = require("./recipes");
 const searchRouter = require("./search");
 
 mainRouter.use("/", welcomeRouter);
-<<<<<<< HEAD
 mainRouter.use("/recipe", recipesRouter )
-=======
 mainRouter.use("/recipes", recipesRouter);
 mainRouter.use("/search", searchRouter);
->>>>>>> a9263afbff65858b8795c3d0eea554feb82f99c2
 
 module.exports = mainRouter;
