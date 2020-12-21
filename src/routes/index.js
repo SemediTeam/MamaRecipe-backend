71702848e74712fb29edf80a66f5e62d4ce0289b
Vsept17 <verzchseptian@gmail.com>
2020-12-22 01:13:07 +0700

    fix conflict

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,10 @@ const mainRouter = require("express").Router();
 
 const welcomeRouter = require("./welcome");
 const recipesRouter = require("./recipes");
-<<<<<<< HEAD
 const searchRouter = require("./search");
 
 mainRouter.use("/", welcomeRouter);
-mainRouter.use("/recipe", recipesRouter )
-mainRouter.use("/recipes", recipesRouter);
-mainRouter.use("/search", searchRouter);
-=======
-
-mainRouter.use("/", welcomeRouter);
 mainRouter.use("/recipe", recipesRouter);
->>>>>>> devsept
+mainRouter.use("/search", searchRouter);
 
 module.exports = mainRouter;
