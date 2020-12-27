187b03936e6e79bc7dd8e374980a01a2a0c88379
Vsept17 <verzchseptian@gmail.com>
2020-12-27 14:14:39 +0700

    add bookmark

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,12 +7,7 @@ const searchRouter = require("./search");
 const userRouter = require("./user.route");
 const likesRouter = require("./likes");
 const commentRouter = require("./comments");
-<<<<<<< HEAD
 const bookmarksRouter = require("./bookmarks")
-=======
-const bookmarkRouter = require("./bookmark");
-
->>>>>>> 161982f06634a3b7c923da6a7259fa6feed18de6
 
 mainRouter.use("/", welcomeRouter);
 mainRouter.use("/auth", authRouter);
@@ -20,12 +15,7 @@ mainRouter.use("/recipe", recipesRouter);
 mainRouter.use("/search", searchRouter);
 mainRouter.use("/user", userRouter);
 mainRouter.use("/likes", likesRouter);
-<<<<<<< HEAD
 mainRouter.use("/bookmarks", bookmarksRouter);
-
-=======
->>>>>>> 161982f06634a3b7c923da6a7259fa6feed18de6
 mainRouter.use("/comments", commentRouter);
-mainRouter.use("/bookmark", bookmarkRouter);
 
 module.exports = mainRouter;
